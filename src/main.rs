15d89db0410b53b48d14a8f76e969df661a6ea36
Uchio Kondo <udzura@udzura.jp>
2021-05-27 23:33:47 +0900

    Initial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+extern crate serde;
+
+use rocket::response::content;
+use rocket_contrib::json::Json;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Ret {
+    status: i32,
+}
+
+#[get("/")]
+fn index() -> Json<Ret> {
+    Json(Ret { status: 200 })
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index]).launch();
 }
