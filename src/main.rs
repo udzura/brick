7aa0948dfa2ececfb17f7f63962a75e1856fe710
Uchio Kondo <udzura@udzura.jp>
2021-05-28 19:11:54 +0900

    Get the status from API

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,13 @@ use rocket::*;
 use rocket_contrib::json::Json;
 use serde::Serialize;
 
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::os::unix::io::AsRawFd;
+use std::os::unix::io::FromRawFd;
 use std::{
     collections::HashMap,
+    rc::Rc,
     sync::{Arc, Mutex, RwLock},
     thread,
     time::Duration,
@@ -26,12 +31,12 @@ struct Ret {
 
 #[derive(Debug, Clone)]
 struct Message {
-    id: i32,
+    id: u32,
     cmd: String,
 }
 
 struct ChanManager {
-    pub retval: Arc<RwLock<HashMap<u32, String>>>,
+    pub retval: Arc<RwLock<HashMap<u32, Mutex<String>>>>,
     pub sender: Arc<channel::Sender<Message>>,
 }
 
@@ -50,14 +55,14 @@ fn enqueue(mng: State<ChanManager>) -> Json<Ret> {
     let id = rand::thread_rng().gen();
     let msg = Message {
         id,
-        cmd: "uname -a".to_string(),
+        cmd: "sha512sum /Users/udzura/Documents/*.jpg".to_string(),
     };
 
     let sender = mng.sender.clone();
     match sender.send(msg) {
         Ok(_) => Json(Ret {
             status: 200,
-            ..Default::default()
+            msg: format!("Enqueued. ID: {}", id).into(),
         }),
         Err(e) => Json(Ret {
             status: 503,
@@ -67,24 +72,88 @@ fn enqueue(mng: State<ChanManager>) -> Json<Ret> {
 }
 
 #[get("/status/<id>")]
-fn status(id: u32) -> Json<Ret> {
-    Json(Ret {
-        status: 200,
-        ..Default::default()
-    })
+fn status(mng: State<ChanManager>, id: u32) -> Json<Ret> {
+    let retval = mng.retval.clone();
+    let x = match retval.read() {
+        Ok(map) => {
+            let got = map.get(&id);
+            Json(Ret {
+                status: 200,
+                msg: format!("Got: {:?}", got).into(),
+            })
+        }
+        Err(e) => Json(Ret {
+            status: 503,
+            msg: format!("Error: {:?}", e).into(),
+        }),
+    };
+    x
 }
 
 fn main() {
     let (s, r) = channel::bounded::<Message>(256);
+    let store = Arc::new(RwLock::new(HashMap::new()));
 
     let mng = ChanManager {
-        retval: Arc::new(RwLock::new(HashMap::new())),
+        retval: store.clone(),
         sender: Arc::new(s),
     };
-
     thread::spawn(move || loop {
+        // clone store in each iteration
+        let store = store.clone();
         match r.recv() {
-            Ok(got) => println!("Got: {:?}", got),
+            Ok(got) => {
+                println!("Got: {:?}", got);
+                thread::spawn(move || {
+                    use std::process::{Command, Stdio};
+
+                    let mut process = match Command::new("bash")
+                        .arg("-c")
+                        .arg(&got.cmd)
+                        .stdout(Stdio::piped())
+                        .spawn()
+                    {
+                        Err(why) => panic!("couldn't spawn: {:?}", why),
+                        Ok(process) => process,
+                    };
+
+                    let fd = process.stdout.as_ref().unwrap().as_raw_fd();
+                    let io = unsafe { std::fs::File::from_raw_fd(fd) };
+                    let mut reader = BufReader::new(io);
+
+                    loop {
+                        let mut line = String::new();
+                        match reader.read_line(&mut line) {
+                            Ok(len) => {
+                                if len > 0 {
+                                    print!("Stdout: {}", line);
+                                    let mut map = store.write().expect("RwLock poisoned");
+                                    map.entry(got.id)
+                                        .or_insert_with(|| Mutex::new(line.clone()));
+                                }
+                            }
+                            Err(e) => {
+                                println!("error attempting to wait: {:?}", e);
+                                break;
+                            }
+                        }
+
+                        match process.try_wait() {
+                            Ok(Some(status)) => {
+                                println!("exited with: {}", status);
+                                break;
+                            }
+                            Ok(None) => {
+                                continue;
+                            }
+                            Err(e) => {
+                                println!("error attempting to wait: {:?}", e);
+                                break;
+                            }
+                        }
+                    }
+                });
+            }
             Err(e) => println!("Some error: {:?}", e),
         };
     });
